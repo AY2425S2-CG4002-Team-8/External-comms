0e7cbdc32b740a720308104680c28c4fb1b860aa
Leo Zheng Rui Darren <leo.darren@gmail.com>
2025-02-08 23:36:38 +0800

    feat: add tcp server class and relay server functionality

--- game_engine.py
+++ game_engine.py
@@ -1,14 +1,24 @@
 import asyncio
+import json
 from mqtt.mqtt_client import MqttClient
 from eval_client import EvalClient
+from relay_server import RelayServer
 
 class GameEngine:
     def __init__(self, secret_key, host, port):
         self.secret_key = secret_key
+
         self.host = host
         self.port = port
+
         self.eval_client_send_buffer = asyncio.Queue()
+        self.visualiser_read_buffer = asyncio.Queue()
+        self.visualiser_send_buffer = asyncio.Queue()
+        self.relay_server_read_buffer = asyncio.Queue()
+        self.relay_server_send_buffer = asyncio.Queue()
+
         self.tasks = []
+
         self.p1 = {
             'hp': 100,
             'bullets': 6,
@@ -31,11 +41,10 @@ class GameEngine:
             host = "175.41.155.178"
             port = 1883
             publish_topic = "M2MQTT_Unity/test"
-            publish_buffer = asyncio.Queue()
-            await publish_buffer.put("Hello Visualiser.")
-            await publish_buffer.put("Hello Visualiser 2.")
+            await self.visualiser_send_buffer.put("Hello Visualiser.")
+            await self.visualiser_send_buffer.put("Hello Visualiser 2.")
             subscribe_topics = ["test/topic", publish_topic]
-            mqtt_client = MqttClient(host, port, publish_buffer, publish_topic, subscribe_topics)
+            mqtt_client = MqttClient(host, port, self.visualiser_send_buffer, publish_topic, subscribe_topics)
             print("Starting MQTT")
             await mqtt_client.run()
         except:
@@ -45,37 +54,22 @@ class GameEngine:
         eval_client = EvalClient(self.secret_key, self.host, self.port, self.eval_client_send_buffer)
         await eval_client.run()
 
-    async def temporary_eval_client_buffer_putter(self):
-        test_data = {
-            "player_id": 1, 
-            "action": "gun", 
-            "game_state": {
-                "p1": {
-                    "hp": 100, 
-                    "bullets": 5, 
-                    "bombs": 2, 
-                    "shield_hp": 0, 
-                    "deaths": 0, 
-                    "shields": 3
-                },
-                "p2": {
-                    "hp": 95, 
-                    "bullets": 6, 
-                    "bombs": 2, 
-                    "shield_hp": 0, 
-                    "deaths": 0, 
-                    "shields": 3
-                },
-            }
-        }
+    async def initiate_relay_server(self):
+        relay_server_port = 8080
+        relay_server = RelayServer(self.secret_key, self.host, relay_server_port, self.relay_server_read_buffer, self.relay_server_send_buffer)
+        await relay_server.run()
+
+    async def game_process(self):
+        """
+        Consumes messages from relay_server_read_buffer and forwards them to eval_client_send_buffer.
+        """
         while True:
-            try:
-                x = await asyncio.to_thread(input)
-                await self.eval_client_send_buffer.put(test_data)
-                print("Data added to EvalClient buffer.")
-            except Exception as e:
-                print(f"temporary_eval_client_buffer_putter task has raised exception {e}.")
-                raise
+            message = await self.relay_server_read_buffer.get()
+            # Run loads as a temporary measure to simulate a new json created by GE
+            message = json.loads(message)
+            print(f"Processing message: {message}")
+            await self.eval_client_send_buffer.put(message)
+            print(f"Queued message to eval_server: {message}")
 
     async def stop(self):
         print("Cancelling tasks...")
@@ -87,7 +81,8 @@ class GameEngine:
         self.tasks = [
             self.initiate_mqtt(),
             self.initiate_eval_client(),
-            self.temporary_eval_client_buffer_putter()
+            self.initiate_relay_server(),
+            self.game_process()
         ]
         try:
             await asyncio.gather(*self.tasks)
---
+++ tcp/tcp_server.py
@@ -0,0 +1,138 @@
+import asyncio
+
+class TcpServer:
+    def __init__(self, secret_key, host, port, read_buffer, send_buffer, timeout=60):
+        self.secret_key = secret_key
+        self.host = host
+        self.port = port
+        self.read_buffer = read_buffer
+        self.send_buffer = send_buffer
+        self.timeout = timeout
+        self.clients = set()
+
+    async def start(self):
+        try:
+            tcp_server = await asyncio.start_server(self.on_client_connect, self.host, self.port)
+            async with tcp_server:
+                await tcp_server.serve_forever()
+
+        except Exception as e:
+            print(f"TCP Server on (host, port): ({self.host}, {self.port}) failed to start.")
+        
+
+    async def on_client_connect(self, reader, writer):
+        success, message = await self.receive_message(reader)
+        if not success:
+            print(f"Client from {writer.get_extra_info('peername')} was not able to connect.")
+            await self.close_connection(writer)
+            return
+
+        host_name = writer.get_extra_info('peername')
+        try:
+            if message != "hello":
+                print(f"Incorrect handshake packet from {host_name}. Got {message} instead")
+                await self.close_connection(writer)
+                return
+            self.add_client(writer)
+            print(f"Successfully received handshake packet from {host_name}")
+
+            await asyncio.gather(
+                asyncio.create_task(self.read_task(reader, writer)),
+                asyncio.create_task(self.write_task(writer))
+            )
+
+        except Exception as e:
+            print(f"Exception on TCP server: {e}")
+    
+    def add_client(self, writer):
+        """
+        Adds writer as a unique identifier for open client sockets
+        """
+        self.clients.add(writer)
+        return
+        
+    
+    def remove_client(self, writer):
+        self.clients.discard(writer)
+        return
+
+    async def receive_message(self, reader):
+        """
+        Returns success and message received from tcp client or server
+        """
+        success, message = False, None
+        try:
+            while True:
+                data = b''
+                while not data.endswith(b'_'):
+                    literal = await reader.read(1)
+                    if not literal:
+                        print(f"Client disconnected unexpectedly")
+                        return success, message
+                    data += literal
+                    
+                if len(data) == 0:
+                    print(f"Packet is empty")
+                    return success, message
+                
+                data = data.decode("utf-8")
+                length = int(data[:-1])
+                success = True
+                message = await reader.readexactly(length)
+                message = message.decode('utf-8')
+                return success, message
+            
+        except asyncio.TimeoutError:
+            print("Timeout while waiting for message.")
+            return success, message
+        except asyncio.IncompleteReadError:
+            print("Incomplete read error. Connection may have been lost.")
+            return success, message
+        except ValueError:
+            print("Invalid message format received.")
+            return success, message
+        except Exception as e:
+            print(f"Error receiving message: {e}")
+            return success, message
+    
+    async def send_message(self, writer, message):
+        try:
+            writer.write(f"{len(message)}_".encode() + message.encode())
+            await writer.drain()
+            print(f"Sent message to client {writer.get_extra_info('peername')}")
+        
+        except Exception as e:
+            print(f"Exception when sending packet to client: {writer.get_extra_info('peername')}, Exception: {e}")
+
+    async def read_task(self, reader, writer):
+        try:
+            while True:
+                success, message = await self.receive_message(reader)
+                if not success:
+                    print(f"Error in reading packet from client: {writer.get_extra_info('peername')}")
+                    break
+                await self.read_buffer.put(message)
+        except Exception as e:
+            print(f"Exception in reading message from client: {writer.get_extra_info('peername')}")
+        finally:
+            self.close_connection(writer)
+
+
+    async def write_task(self, writer):
+        try:
+            message = await self.send_buffer.get()
+            await self.send_message(writer, message)
+        except Exception as e:
+            print(f"Not able to start write task in TCP server")
+
+    async def close_connection(self, writer):
+        host_name = writer.get_extra_info('peername')
+        try:
+            if not writer.is_closing():
+                writer.close()
+                await writer.wait_closed()
+            self.remove_client(writer)
+            print(f"Successfully disocnnected client: {host_name}")
+        
+        except Exception as e:
+            print(f"Failed to close client connection from: {host_name}")
