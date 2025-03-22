fb70b7d741cc22a291fcc61edffd0af6b3b93f6e
Leo Zheng Rui Darren <leo.darren@gmail.com>
2025-03-23 01:16:05 +0800

    fix: bomb parameters

--- game_state.py
+++ game_state.py
@@ -47,7 +47,7 @@ class GameState:
         elif action == "reload":
             action_possible = attacker.reload()
         elif action == "bomb":
-            action_possible = attacker.bomb(opponent, fov, snow_number)
+            action_possible = attacker.bomb(opponent, fov)
         elif action in {"badminton", "golf", "fencing", "boxing"}:
             attacker.harm_AI(opponent, fov)
         else:
